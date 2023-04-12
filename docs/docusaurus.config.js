b48a58d2b0075eeaeec0dc051378b800edd9a0b2
krillboi <59022282+krillboi@users.noreply.github.com>
2023-04-12 13:32:20 +0200

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -26,7 +26,23 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('swc-loader'),
+      options: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        },
+      },
+    }),
+  },
   presets: [
     [
       'classic',
